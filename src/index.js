2bd20b8b80f0686f49c0c79e232e740d0f322f87
Joseph Ochoa <josephochoa91@gmail.com>
2022-08-16 14:01:01 -0700

    Reset: updated git mistakes

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const divContainer = document.getElementById('root');
+const root = ReactDOM.createRoot(divContainer);
+
+root.render(<App />);
